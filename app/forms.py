ba16b88e1fd471120145dac75f823e459fb5c4be
harshad116 <harshadvali1154@gmail.com>
2021-05-03 19:21:15 -0700

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,19 @@
 from django import forms
-
+g=[['MALE','Male'],['FEMALE','Female']]
+c=[['Django','DJANGO'],['python','PYTHON'],['sql','SQL']]
 class NameForm(forms.Form):
     name=forms.CharField(max_length=100)
-    age=forms.IntegerField(max_value=80,min_value=18)
+    #age=forms.IntegerField(max_value=80,min_value=18)
+    #email=forms.EmailField(max_length=100)
+    #url=forms.URLField()
+    #date=forms.DateField()
+    #date1=forms.DateTimeField()
+    #time=forms.TimeField()
+    password=forms.CharField(max_length=100,widget=forms.PasswordInput())
+    address=forms.CharField(max_length=200,widget=forms.Textarea(attrs={'cols':5,'rows':5}))
+    gender=forms.ChoiceField(choices=g)
+    course=forms.MultipleChoiceField(choices=c)
+    gender2=forms.ChoiceField(choices=g,widget=forms.RadioSelect)
+    course2=forms.MultipleChoiceField(choices=c,widget=forms.CheckboxSelectMultiple())
+
 
--- app/views.py
+++ app/views.py
@@ -9,6 +9,20 @@ def djangoForm(request):
     if request.method=='POST':
         form_data=NameForm(request.POST)
         if form_data.is_valid():
-            #return HttpResponse(str(form_data.cleaned_data))
-            return HttpResponse(form_data.cleaned_data['name'])
+            name=form_data.cleaned_data['name']
+            #age=form_data.cleaned_data['age']
+            #email=form_data.cleaned_data['email']
+            #url=form_data.cleaned_data['url']
+            #date=form_data.cleaned_data['date']
+            #date1=form_data.cleaned_data['date1']
+            #time=form_data.cleaned_data['time']
+            password=form_data.cleaned_data['password']
+            address=form_data.cleaned_data['address']
+            gender=form_data.cleaned_data['gender']
+            course=form_data.cleaned_data['course']
+            #d={'name':name,'age':age,'email':email,'url':url,'date':date,'date1':date1,'time':time}
+            d1={'name':name,'password':password,'address':address,'gender':gender,'course':course}
+            return render(request,'formdata.html',context=d1)
+            
+
     return render(request,'temp1.html',context={'form':form})
